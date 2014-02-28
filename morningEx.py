273424aeffda52a5936d85e59074b7041497d946
wilsonlj <wilson_lj@msn.com>
2014-02-28 13:02:11 +0100

    second commit

---
+++ morningEx.py
@@ -0,0 +1,21 @@
+import time
+
+def countDown(n):
+	for i in xrange(n+1):
+		print n-i
+		time.sleep(1)
+
+if __name__ == '__main__':
+	repeat = 12
+	train = 40
+	relax = 20
+	countDown(10)
+	print '\nStart!'
+	for i in xrange(repeat+1):
+		print 'Ex',i+1
+		print "\a\a\a\a\a"
+		countDown(train)
+		print "\a\a\a"
+		print "Relax..."
+		countDown(relax)
+	print '\nFinished!'
