378ef2b2f38050b392841986c9525b1b04b4eea3
wilsonlj <wilson_lj@msn.com>
2014-03-07 18:13:17 +0100

    paradigm

--- morningEx.py
+++ morningEx.py
@@ -1,18 +1,20 @@
+# -*- coding: utf-8 -*-
 import time
 
 def countDown(n):
-	for i in xrange(n+1):
-		print n-i
+	for i in xrange(n + 1):
+		print n - i
 		time.sleep(1)
 
 def main():
 	repeat = 12
 	train = 40
 	relax = 20
+	
 	countDown(10)
 	print '\nStart!'
-	for i in xrange(repeat+1):
-		print 'Ex',i+1
+	for i in xrange(repeat + 1):
+		print 'Ex', i + 1
 		print "\a\a\a\a\a"
 		countDown(train)
 		print "\a\a\a"
